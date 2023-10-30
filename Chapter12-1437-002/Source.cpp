6136f09cd8468c1200342964afe121f92192a815
Work <Work@MSI>
2023-10-30 15:37:32 -0500

    Where we ended on Oct 30 in the latter class.

--- Chapter12-1437-002/Source.cpp
+++ Chapter12-1437-002/Source.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+void demoPassByReference(int& a)
+{
+    cout << a << endl; 
+}
+
+
 int main()
 {
-    cout << "Hello World";
+    int a = 51; 
+
+
+    cout << &a << endl; 
+
+    int* pointerToA = &a; 
+    cout << pointerToA << endl; 
+    //cout << a * a << endl; 
+
+    //if (a > 0 & a < 100)
+    //cout << a << endl; 
+
+    cout << *pointerToA << endl; //* acts as the DEREFERENCING operator here 
 
     return 0;
 }
